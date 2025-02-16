30178ef1c7e7bc7e4c87ddc8e768e46b1242af4d
Abhishek Pandey <96662569+AbhishekChetiya@users.noreply.github.com>
2025-02-17 03:36:03 +0530

    Still Facing Problem

--- Backend/index.js
+++ Backend/index.js
@@ -49,7 +49,7 @@ app.listen(PORT, () => {
 // Middleware
 app.use(cors(
   {
-    origin: ['https://ai-movie-character-chatbot.vercel.app', "https://another-frontend.com"],
+    origin: ['https://ai-movie-character-chatbot.vercel.app', "http://localhost:5174/"],
     credentials: true
   }
 ));
